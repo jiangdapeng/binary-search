c9d8e01b7ec6038f14265db4f0e92ab7eb9cb913
Dapeng Jiang <asuwill.jdp@gmail.com>
2012-03-15 23:12:45 +0800

    add bitsort.cpp

---
+++ bitsort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+static int MASK = 1;
+static const int BLOCK_SIZE=32;
+static const int BUFF_SIZE=10^7;
+static int bg[BUFF_SIZE/BLOCK_SIZE+1];
+
+void clear()
+{
+	memset(bg,0,sizeof(bg)/sizeof(bg[0]));	
+}
+
+void set(int n)
+{
+	bg[n/BLOCK_SIZE] |= MASK << (n%BLOCK_SIZE);
+}
+
+bool test(int n)
+{
+	return (bg[n/BLOCK_SIZE] & (MASK <<  (n%BLOCK_SIZE)))!=0 ;
+}
+
+int main()
+{
+	ifstream data("data.txt");
+	int tmp;
+	int max=0;
+	clear();
+	while(data>>tmp)
+	{
+		cout<<tmp<<endl;
+		set(tmp);
+		max=tmp>max?tmp:max;
+	}
+	ofstream result("r.txt");
+	for(int i=0;i<=max;++i)
+	{
+		cout<<"i="<<i<<endl;
+		if(test(i))
+		{
+			result<<i<<endl;
+		}
+	}
+}
