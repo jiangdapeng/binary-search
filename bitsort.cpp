884b88f12f2d030cf1a1e2da9c00f32e19615572
Dapeng Jiang <asuwill.jdp@gmail.com>
2012-03-16 00:00:19 +0800

    add comments in bitsort.cpp

--- bitsort.cpp
+++ bitsort.cpp
@@ -8,16 +8,26 @@ static const int BLOCK_SIZE=32;
 static const int BUFF_SIZE=10^7;
 static int bg[BUFF_SIZE/BLOCK_SIZE+1];
 
+/**
+ * clear all bits to zero
+ * */
 void clear()
 {
 	memset(bg,0,sizeof(bg)/sizeof(bg[0]));	
 }
 
+/**
+ * set bits n to 1
+ * n counting from zero
+ */
 void set(int n)
 {
 	bg[n/BLOCK_SIZE] |= MASK << (n%BLOCK_SIZE);
 }
 
+/**
+ * test whether bit n is 1
+ * /
 bool test(int n)
 {
 	return (bg[n/BLOCK_SIZE] & (MASK <<  (n%BLOCK_SIZE)))!=0 ;
